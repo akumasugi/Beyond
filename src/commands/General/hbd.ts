46de3cd52e44f9836b004b50064ad5f6c44b95af
akumazero6 <102878540+akumazero6@users.noreply.github.com>
2022-04-27 13:04:25 +0530

    Update hbd.ts

--- src/commands/General/hbd.ts
+++ src/commands/General/hbd.ts
@@ -31,7 +31,7 @@ export default class Command extends BaseCommand {
         } catch (err) {
             M.reply(`Profile Picture not Accessible of ${username}`)
             pfp =
-                'https://wallpaperaccess.com/full/5304840.png'
+                'https://i.ibb.co/PhCXwg2/370736967beee002dadafe7c4d2ed107.jpg'
         }
         await M.reply(
             await request.buffer(
