3461c35495db775e5b820f2346e4af3ec7090740
akumazero6 <102878540+akumazero6@users.noreply.github.com>
2022-04-27 13:46:46 +0530

    Update Party.ts

--- src/commands/Pokemon/Party.ts
+++ src/commands/Pokemon/Party.ts
@@ -50,7 +50,7 @@ export default class Command extends BaseCommand {
     }
     const buttonMessage: any = {
       contentText: `${text}`,
-      footerText: "MARIN💕",
+      footerText: "MARIN💖",
       buttons: buttons,
       headerType: 1,
     };
